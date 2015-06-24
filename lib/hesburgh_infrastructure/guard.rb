3208c3e07308dfcb9c9fa6ae9364f8221a391577
Jaron Kennel <jaronkk@gmail.com>
2015-06-24 10:18:50 -0400

    Disabled full spec run when spec helper changes

--- lib/hesburgh_infrastructure/guard.rb
+++ lib/hesburgh_infrastructure/guard.rb
@@ -85,7 +85,8 @@ module HesburghInfrastructure
 
         guard 'spring', options do
             watch(%r{^spec/.+_spec\.rb$})
-            watch(%r{^spec/spec_helper\.rb$})                   { |m| 'spec' }
+            # Don't run all specs when spec_helper changes
+            # watch(%r{^spec/spec_helper\.rb$})                   { |m| 'spec' }
             watch(%r{^app/(.+)\.rb$})                           { |m| "spec/#{m[1]}_spec.rb" }
             watch(%r{^lib/(.+)\.rb$})                           { |m| "spec/lib/#{m[1]}_spec.rb" }
             watch(%r{^app/controllers/(.+)_(controller)\.rb$})  do |m|
